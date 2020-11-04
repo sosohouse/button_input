a95de144565ef023d57f4931add243572601060f
sosohouse <72775620+sosohouse@users.noreply.github.com>
2020-11-04 09:14:04 +0900

    A button led output text "A"

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("A+B")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+})
 basic.forever(function () {
 	
 })
